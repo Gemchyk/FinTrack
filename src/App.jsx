8cc42a46925e1549377385ef27b0d620354cbaf1
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-06-06 15:50:25 +0300

    added feature/expensesGoalsByCategory

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import WeeklyComparison from './components/WeeklyComparison';
 import TotalBalance from './components/TotalBalance/TotalBalance';
+import ExpensesGoalsByCategory from './components/ExpensesGoals/ExpensesGoalsByCategory';
 
 function App() {
 
   return (
     <>
         <TotalBalance />
-      {/* <WeeklyComparison /> */}
+        {/* <WeeklyComparison /> */}
+        {/* <ExpensesGoalsByCategory /> */}
     </>
   )
 }
