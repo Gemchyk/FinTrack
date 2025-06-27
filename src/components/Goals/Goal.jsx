624c279141d001040fb63d40fc8cbdfef269ecab
Gemchyk <gemlifestudio@gmail.com>
2025-06-27 17:40:34 +0300

    dynamic categories dashboard added

--- src/components/Goals/Goal.jsx
+++ src/components/Goals/Goal.jsx
@@ -7,19 +7,13 @@ import './Goal.scss'
 function Goal(props) {
     return (
         <div>
-            <h1 className='title'>Goals</h1>
+            <h1 className='title'>Spendings</h1>
                 <div className='main'>
                     <div className='main-header'>
-                        <h4 className='main-sec-title'>2025</h4>
+                        <h4 className='main-title'>Categories Spendings 2025</h4>
                     </div>
                     <div className='goals-details-div'>
-                        <div>
-                        
-                            
-                        </div>
-                        <div>
                             <PiePart /> 
-                        </div>
                     </div>
                 </div>
             
